9610a643d4aebbf0411a225b69e5673487560c62
hcdias <hugo_root@yahoo.com.br>
2019-02-05 14:16:00 -0200

    Adicionar manipulacao de header,duplicar requisicao xhr e adicionar scroll apos print da pagina

--- popup.js
+++ popup.js
@@ -1,9 +1,22 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-      code:'var url = "'+tab.url+'";'+
-      'var cleanUrl = url.substring(url.lastIndexOf("http"));'+
-      'var xhr = new XMLHttpRequest();'+
-      'xhr.open("GET",cleanUrl);'+
-      'xhr.onload = function(){document.write(xhr.response);};xhr.send();'
+      code:
+      'navigator.__defineGetter__("userAgent",function(){return "Googlebot/2.1 (+http://www.googlebot.com/bot.html)"});'+
+      'navigator.__defineGetter__("appName",function(){return "Googlebot"});'+
+      'console.log(navigator.userAgent);'+
+      'console.log(navigator.appName);'+
+      'e = function(){'+
+      	'var url = "'+tab.url+'";'+
+      	'var cleanUrl = url.substring(url.lastIndexOf("http"));'+
+      	'var xhr = new XMLHttpRequest();'+
+      	'xhr.open("GET",cleanUrl);'+
+	    'xhr.onload = function(){'+
+	    	'document.write(xhr.response);'+
+	    '};'+
+      	'xhr.send();'+
+      '};'+
+      'e();'+
+      'setTimeout(function(){e();}.bind(this),4000);'+
+      'setTimeout(function(){document.getElementsByClassName("site")[1].scrollIntoView();},5000);'
     });
 });
