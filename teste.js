46f83c7f7efb917e2cd9f67032f47ebc52958d5c
Hugo Carvalho <hugo_root@yahoo.com.br>
2019-03-30 13:49:42 -0300

    Remover arquivo desnecessario

--- teste.js
+++
@@ -1,8 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET","http://www1.folha.uol.com.br/poder/2015/12/1718219-mbl-distribui-panfletos-em-feira-livre-de-favela-de-sp.shtml");
-
-xhr.onload = function(){
-	document.write(xhr.response);
-}
-
-xhr.send();
